bf6fc46f282e758581c7b84af32cc01de43685a9
arie <v005ab.nichiasa@gmail.com>
2023-04-27 09:44:55 +0900

    [add] spendingEditメソッド追加

--- src/main/java/com/example/demo/controller/PaymentsController.java
+++ src/main/java/com/example/demo/controller/PaymentsController.java
@@ -12,9 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.entity.Spending;
 import com.example.demo.form.CreateSpendingForm;
+import com.example.demo.form.EditSpendingForm;
 import com.example.demo.service.CreateSpendingService;
+import com.example.demo.service.EditSpendingService;
+import com.example.demo.service.FindSpendingService;
+import com.example.demo.service.GetAllSpendingsService;
 
 import jakarta.validation.Valid;
 
@@ -24,7 +30,16 @@ import jakarta.validation.Valid;
 public class PaymentsController {
 	
 	@Autowired
-	private CreateSpendingService createSpendingService;	
+	private CreateSpendingService createSpendingService;
+	
+	@Autowired
+	private FindSpendingService findSpendingService;
+	
+	@Autowired
+	private EditSpendingService editSpendingService;
+	
+	@Autowired
+	private GetAllSpendingsService getAllSpendingsService;
 	
 	@GetMapping("/create")
 	public String create(Model model) {
@@ -45,5 +60,30 @@ public class PaymentsController {
 		}
 		createSpendingService.create(form);
 		return "payments/create";
+	}
+	
+	@GetMapping("/spending_edit")
+	public String spendingEdit(@RequestParam int id, EditSpendingForm editSpendingForm, Model model) {
+		//Spending型にgetFindByIdの戻り値を格納
+	    Spending spending = findSpendingService.getFindById(id);
+	    //spendingr型にSpending型の情報を入れかえる
+	    editSpendingForm.setId(spending.getId());
+	    editSpendingForm.setCategoryId(spending.getCategoryId());
+	    editSpendingForm.setAmount(spending.getAmount());
+	    editSpendingForm.setUserId(spending.getUserId());
+        model.addAttribute("editSpendingForm", editSpendingForm);
+		return "payments/spending_edit";
+	}
+	
+	@PostMapping("/spending_edit")
+	public String spendingEdit(@Valid @ModelAttribute("editUser") EditSpendingForm editSpendingForm, BindingResult result, Model model) {
+		List<Spending> spendings = getAllSpendingsService.getAllSpendings();
+		if (result.hasErrors()) {
+            model.addAttribute("editSpendingForm", editSpendingForm);
+            return "payments/spending_edit";
+        }
+		model.addAttribute("spendings", spendings);
+		editSpendingService.edit(editSpendingForm);
+		return "payments/spending_edit";
 	}
 }
