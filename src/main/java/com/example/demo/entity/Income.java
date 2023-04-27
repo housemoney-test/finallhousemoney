3612740b27d093527c94d5a35a53bf5a4d71852c
arie <v005ab.nichiasa@gmail.com>
2023-04-27 14:07:24 +0900

    入金登録　各種クラスファイル追加

---
+++ src/main/java/com/example/demo/entity/Income.java
@@ -0,0 +1,46 @@
+package com.example.demo.entity;
+
+public class Income {
+
+    private int id;
+
+    private int categoryId;
+    
+    private int amount;
+    
+    private int userId;
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+    
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    
+}
