a96a1501a7aa9ce50d6c98093d4b63474803ecf8
yasunakayayoi <yasunaka.yayoi@alhinc.jp>
2023-04-28 11:02:10 +0900

    [fix]TopController.java

--- src/main/java/com/example/demo/controller/TopController.java
+++ src/main/java/com/example/demo/controller/TopController.java
@@ -35,6 +35,7 @@ public class TopController {
 
 	@GetMapping("/login")
 	public String login(Model model) {
+		//フォームの型指定
 		model.addAttribute("loginUserForm", new LoginUserForm());
 		return "top/login";
 	}
@@ -56,8 +57,8 @@ public class TopController {
 
 		//パスが一致している場合
 		if (loginUser != null && encoder.matches(loginUserForm.getPassword(), loginUser.getPassword())) {
-			//セッションにユーザー情報を保持する
-			session.setAttribute("user", session);
+			// 認証が成功した場合、ログインされたUserオブジェクトをセッションに設定する
+		    session.setAttribute("user", loginUser);
 			return "redirect:/users/home";
 
 		} else {
