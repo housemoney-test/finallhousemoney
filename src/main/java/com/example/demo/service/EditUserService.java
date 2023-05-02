24b74766ee8969ef48d5fa35f74c3c187d36bf49
arie <v005ab.nichiasa@gmail.com>
2023-05-02 11:10:33 +0900

    マッパーに追記

--- src/main/java/com/example/demo/service/EditUserService.java
+++ src/main/java/com/example/demo/service/EditUserService.java
@@ -4,24 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.demo.entity.FixedCost;
-import com.example.demo.form.EditFixedCostForm;
+import com.example.demo.entity.User;
+import com.example.demo.form.EditUserForm;
 import com.example.demo.mapper.UserMapper;
 
 @Service
 public class EditUserService {
     
 	 @Autowired
-	    private UserMapper fixedCostMapper;
+	    private UserMapper userMapper;
 
 	    @Transactional
-	    public int edit(EditFixedCostForm editFixedCostForm) {
-	        FixedCost fixedCost = new FixedCost();
-	        fixedCost.setId(editFixedCostForm.getId());
-	        fixedCost.setCategoryId(editFixedCostForm.getCategoryId());
-	        fixedCost.setAmount(editFixedCostForm.getAmount());
-	        fixedCost.setUserId(editFixedCostForm.getUserId());
-	        return fixedCostMapper.edit(fixedCost);
+	    public int editIncome(EditUserForm editUserForm) {
+	        User user = new User();
+	        user.setIncome(editUserForm.getIncome());
+	        return userMapper.editIncome(user);
 	    }
     
 }
