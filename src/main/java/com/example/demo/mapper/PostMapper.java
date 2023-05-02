84dc47f8911a8d5b8a686e32fd392dd6d567ff3c
mimura15 <5101south@gmail.com>
2023-05-02 15:16:44 +0900

    add post一覧

--- src/main/java/com/example/demo/mapper/PostMapper.java
+++ src/main/java/com/example/demo/mapper/PostMapper.java
@@ -1,9 +1,15 @@
 package com.example.demo.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
 import com.example.demo.entity.Post;
+import com.example.demo.entity.User;
 
 @Mapper
 public interface PostMapper {
     void create(Post entity);
+    
+    List<Post> getAll(User entity);
 }
